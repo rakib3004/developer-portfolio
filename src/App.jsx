90405ec89331f205cc9c642fe12e3b1907d86800
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2023-07-25 16:36:44 +0600

    implement motion

--- src/App.jsx
+++ src/App.jsx
@@ -23,9 +23,9 @@ const App = () => {
           <Hero />
         </div>
 
-        {/* <About />
-        <Experience />
-        <Tech />
+        <About />
+       <Experience />
+        {/*  <Tech />
         <Works />
         <Feedbacks /> */}
         <div className="relative z-0">
