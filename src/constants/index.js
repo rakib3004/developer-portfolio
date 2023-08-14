55e4a32b92f27ab230d842498abb73982040312e
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2023-08-14 16:11:45 +0600

    complete description, testimonial and projects in my portfolio

--- src/constants/index.js
+++ src/constants/index.js
@@ -149,10 +149,10 @@ import {
       iconBg: "#383E56",
       date: "Jul 2021 - Feb 2022",
       points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
+        "Strong foundation in HTTP fundamentals, REST API concepts, and proficient knowledge of status codes and CORS for robust web application development.",
+        "Expertise in JavaScript and ES6, with the ability to leverage these skills for creating dynamic and interactive user experiences.",
+        "Familiarity with Vue.js basics, demonstrated through the development of a functional to-do app, showcasing hands-on experience in front-end frameworks.",
+        "Well-versed in web protocols, emphasizing a holistic understanding of web technologies for effective and efficient development practices.",
       ],
     },
     {
@@ -162,10 +162,10 @@ import {
       iconBg: "#E6DEDD",
       date: "Dec 2019 - Present",
       points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
+        "Extensive experience in UI/UX design, adeptly contributing to Research and Development initiatives for innovative user interfaces.",
+        "Proficient in crafting comprehensive SRS documentation and leveraging React.js and Express.js to create dynamic and responsive web applications.",
+        "Skilled in deploying AWS services, managing Redis, and optimizing software with Docker, showcasing prowess in cloud computing and containerization.",
+        "Well-versed in networking, databases, operating systems, and machine learning, complemented by strong project management acumen in computer science and software engineering realms.",
       ],
     },
   ];
@@ -173,42 +173,42 @@ import {
   const testimonials = [
     {
       testimonial:
-        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-      name: "Sara Lee",
-      designation: "CFO",
-      company: "Acme Co",
+      "DevOps virtuoso, transforming deployments with Docker and AWS mastery, while solving intricate networking and database challenges.",      name: "Sara Lee",
+      name: "Mr. X",
+      designation: "HR",
+      company: "Renova IT",
       image: "https://randomuser.me/api/portraits/women/4.jpg",
     },
     {
       testimonial:
-        "I've never met a web developer who truly cares about their clients' success like Rick does.",
-      name: "Chris Brown",
-      designation: "COO",
-      company: "DEF Corp",
+      "UX/UI maven, elevating experiences through React.js and Express.js brilliance, complemented by meticulous SRS documentation and stellar project management.",
+      name: "Mr. Y",
+      designation: "Web Developer",
+      company: "Metasoft",
       image: "https://randomuser.me/api/portraits/men/5.jpg",
     },
     {
       testimonial:
-        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-      name: "Lisa Wang",
-      designation: "CTO",
-      company: "456 Enterprises",
+      "Tech polymath, seamlessly navigating machine learning and software engineering realms, adeptly leveraging Redis, AWS, and a visionary mindset.",
+      name: "Mr. Z",
+      designation: "Tech Lead",
+      company: "Skill Station 19",
       image: "https://randomuser.me/api/portraits/women/6.jpg",
     },
   ];
   
   const projects = [
     {
-      name: "Car Rent",
+      name: "Bloging Platform",
       description:
-        "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+        "Empowering users to share thoughts and ideas through an intuitive and feature-rich online platform.",
       tags: [
         {
           name: "react",
           color: "blue-text-gradient",
         },
         {
-          name: "mongodb",
+          name: "expressjs",
           color: "green-text-gradient",
         },
         {
@@ -220,20 +220,20 @@ import {
       source_code_link: "https://github.com/",
     },
     {
-      name: "Job IT",
+      name: "Campus Canteen",
       description:
-        "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+        "Enhancing campus dining experiences by offering a seamless food delivery system tailored for students and staff.",
       tags: [
         {
           name: "react",
           color: "blue-text-gradient",
         },
         {
-          name: "restapi",
+          name: "nest-js",
           color: "green-text-gradient",
         },
         {
-          name: "scss",
+          name: "postgresql",
           color: "pink-text-gradient",
         },
       ],
@@ -241,20 +241,20 @@ import {
       source_code_link: "https://github.com/",
     },
     {
-      name: "Trip Guide",
+      name: "Catering Express",
       description:
-        "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+        "Catering Express: Streamlining catering operations with a comprehensive management system for efficient event planning and execution.",
       tags: [
         {
-          name: "nextjs",
+          name: "reactjs",
           color: "blue-text-gradient",
         },
         {
-          name: "supabase",
+          name: "sprint-boot",
           color: "green-text-gradient",
         },
         {
-          name: "css",
+          name: "redis",
           color: "pink-text-gradient",
         },
       ],
